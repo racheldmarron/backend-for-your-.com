41124f6c81fad3cb4f7b43ec28b6a0e4e0db9d2a
Rachel Marron <racheldmarron@gmail.com>
2022-03-25 11:09:54 -0700

    fixed .env file so code is working now, tested database through insomnia and passed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'category_id']
       }
     ]
   })
@@ -31,7 +31,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Product,
-        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        attributes: ['id', 'product_name', 'price', 'category_id']
       }
     ]
   })
